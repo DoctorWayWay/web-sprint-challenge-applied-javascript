097174a3ff08468857e595d1c11b60ed261cf812
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-01 06:51:31 -0700

    Completed tasks 4 and 5 in tabs.js. Added a component to create tabs, along with a function that gets an object from an API and applies it to the component function, which then shows its result on the site page

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,7 +15,22 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
+
+  // Creating new element with class (only topicContainer)
+  const topicContainer = document.createElement("div");
+  topicContainer.classList.add("topics");
+
+  // Looping through "topics" array to create desired elements, set up their classes, add content, and appended them to the topicContainer
+  topics.forEach((topic) => {
+    const topicTab = document.createElement("div");
+    topicTab.classList.add("tab");
+    topicTab.textContent = topic;
+    topicContainer.appendChild(topicTab);
+  });
+
+  // Returning topicContainer
+  return topicContainer;
+};
 
 const tabsAppender = (selector) => {
   // TASK 4
@@ -23,6 +40,20 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
 
-export { Tabs, tabsAppender }
+  // Pointing to the selector
+  const tabsInjector = document.querySelector(selector);
+
+  // Axios function
+  axios
+    .get(`http://localhost:5000/api/topics`)
+    .then((responce) => {
+      const tabsData = responce.data.topics;
+      tabsInjector.appendChild(Tabs(tabsData));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export { Tabs, tabsAppender };
